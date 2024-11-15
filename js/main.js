c6b2c0ce970ec2a8eb88f6a2b3311c035d694e6a
sa001gar <kundusagar233@gmail.com>
2024-11-15 12:30:30 +0530

    Update

--- js/main.js
+++ js/main.js
@@ -139,6 +139,29 @@ $(".service-carousel").owlCarousel({
             }
         }
     });
+
+
+    $(".team-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+    
     
 })(jQuery);
 
